77011abcb51e12b1bc84daaa268d7cf8386e6dc1
Rodolfo <rodolfo.novak@gmail.com>
2013-01-11 11:20:11 -0500

    title

--- index.php
+++ index.php
@@ -40,6 +40,24 @@
         <div id="content">
           <div id="logo"></div>
           <h1>Code for Canada</h1>
+          <div>
+              <!-- Begin MailChimp Signup Form -->
+              <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+              <style type="text/css">
+                #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+              </style>
+              <div id="mc_embed_signup">
+              <form action="http://ripeapps.us1.list-manage.com/subscribe/post?u=45f08dd991a660cced47282ce&amp;id=0779908e55" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+              </form>
+              </div>
+
+              <!--End mc_embed_signup-->
+          </div>
         </div>
       </div>
   </body>
